3b773944bc74a790550963363d5a2d6da3d895bf
Arief Adjie Wicaksono <id.ariefadjie@gmail.com>
2019-11-17 11:30:59 +0700

    add 'selamat datang' and remove text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="starter-template">
         <h1>Home</h1>
+        <p>Selamat Datang</p>
         <p class="lead">Ini adalah halaman home</p>
-        <p>Selamat bersenang-senang disini.</p>
     </div>
 @endsection
